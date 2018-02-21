28ff1e2956e714b97daa3bc23e549f760ec7aa29
Kamil <ushurbakiev.k@gmail.com>
2018-02-22 05:01:42 +0600

    'HomeWork_07_Task_03'

---
+++ HomeWork_07_Task_03/HomeWork_07_Task_03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*2.	Дан класс сожержащий внутри себя массив. 
+ * Реализовать перегрузку операторов < , > так, чтобы если сумма элементов массива 1 класса больше, 
+ * возвращалось значение true и наоборот.*/
+
+namespace HomeWork_07_Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t1 = new Test(10);
+            Test t2 = new Test(15);
+
+            Console.WriteLine("t1 > t2: {0}", t1 > t2);
+            Console.WriteLine("t1 < t2: {0}", t1 < t2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork_07_Task_03/HomeWork_07_Task_03/Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_07_Task_03
+{
+    public class Test
+    {
+        public static Random rnd = new Random();
+        public int[] elements { private set; get; }
+
+        public Test(int size)
+        {
+            elements = new int[size];
+
+            //заполняем автоматически для тестирования
+            for (int i = 0; i < size; i++)
+            {
+                elements[i] = rnd.Next(1, 100);
+            }
+        }
+
+        public static bool operator >(Test t1, Test t2)
+        {
+            return t1.Summ() > t2.Summ();
+        }
+
+        public static bool operator <(Test t1, Test t2)
+        {
+            return t1.Summ() < t2.Summ();
+        }
+
+
+        //метод для подсчета суммы элементов
+        private long Summ()
+        {
+            long summ = 0;
+
+            foreach (int value in elements)
+                summ += value;
+
+            return summ;
+        }
+    }
+}
