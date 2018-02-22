fd61a078b0f1a89c33334f92d389ec7a9dc28b54
Kamil <ushurbakiev.k@gmail.com>
2018-02-22 07:25:16 +0600

    'HomeWork_09_Task_02'

---
+++ HomeWork_09_Task_02/HomeWork_09_Task_02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ ДОПОЛНИТЕЛЬНО.
+1.	Создать интерфейс IStudent: 
+●	string Name { get; set; }
+●	string FullName { get; set; }
+●	int[] Grades { get; set; }
+●	String GetName()
+●	String GetFullName() 
+●	Double GetAvgGrade() 
+Наследовать от интерфейса класс Student и реализовать его.
+*/
+
+namespace HomeWork_09_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork_09_Task_02/HomeWork_09_Task_02/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_09_Task_02
+{
+    public class Student : IStudent
+    {
+        public string Name { set; get; }
+        public string FullName { set; get; }
+        public int[] Grades { set; get; }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public string GetFullName()
+        {
+            return this.FullName;
+        }
+
+        public double GetAvgGrade()
+        {
+            int summ = 0;
+
+            foreach (int grade in Grades)
+                summ += grade;
+            return (double)summ / Grades.Length;
+        }
+    }
+}
